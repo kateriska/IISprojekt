0dbf290485218348bf59c54f642600d5ba2e96cf
mazanec <ondrejholub202@gmail.com>
2019-10-20 14:10:06 +0200

    experiment

--- index.php
+++ index.php
@@ -2,10 +2,12 @@
 <head>
   <link href="https://fonts.googleapis.com/css?family=Montserrat&display=swap&subset=latin-ext" rel="stylesheet">
   <link rel="stylesheet" href="style.css">
-  <!--
+  <?php 
+    require_once('fn.elements.php');  
+  ?>
   <style>
     body{color: blue; background-color: yellow}
-  </style>-->
+  </style>
 </head>
 
 
