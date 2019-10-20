2f67d588edb5cdf1737ae4ae9d42aa32658ae7bb
mazanec <ondrejholub202@gmail.com>
2019-10-20 23:21:40 +0200

    commented out show_*()

--- index.php
+++ index.php
@@ -15,11 +15,11 @@
 
   <container class="center">
 <?php 
-  show_pending_student_registrations();       //G
-  show_my_courses_student();                  //S
-  show_my_courses_lecturer();                 //L
-  show_my_courses_garant();                   //G
-  tile_manage_rooms();                        //V
+  //TODO show_pending_student_registrations();       //G
+  //TODO show_my_courses_student();                  //S
+  //TODO show_my_courses_lecturer();                 //L
+  //TODO show_my_courses_garant();                   //G
+  tile_manage_rooms();                        //M
   tile_manage_users();                        //A
   tile_show_all_courses();                    //N
   tile_edit_profile();                        //S
