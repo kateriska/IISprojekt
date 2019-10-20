681d1a9667e99f54e44cf332105c6bebe07baafb
mazanec <ondrejholub202@gmail.com>
2019-10-20 17:15:20 +0200

    experiment

--- fn.elements.php
+++ fn.elements.php
@@ -6,9 +6,9 @@ function insert_tile($name, $url){
 function insert_login_bar(){
   echo("<div id='login_bar'>
         <form id='login' action='act.login.php' method='post'>
-          <input type="text" name="mail" placeholder="Email">
-          <input type="password" name="pwd" placeholder="Heslo">
-          <button type="submit" name="submit_login">Přihlásit</button>
+          <input type='text' name='mail' placeholder='Email'>
+          <input type='password' name='pwd' placeholder='Heslo'>
+          <button type='submit' name='submit_login'>Přihlásit</button>
         </form>
         </div>");
 }
