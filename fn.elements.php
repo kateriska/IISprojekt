c7b68672c4135d3cf62597bcb5f64d68e0dd0fa3
mazanec <ondrejholub202@gmail.com>
2019-10-20 17:54:23 +0200

    experiment

--- fn.elements.php
+++ fn.elements.php
@@ -4,13 +4,13 @@ function insert_tile($name, $url){
 }
 
 function insert_login_bar(){
-  echo("<div id='login_bar'>
+  echo("<container id='login_bar'>
         <form id='login' action='act.login.php' method='post'>
           <input type='text' name='mail' placeholder='Email'>
           <input type='password' name='pwd' placeholder='Heslo'>
           <button type='submit' name='submit_login'>Login</button>
         </form>
-        </div>");
+        </container>");
 }
 
 
