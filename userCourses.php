21dbb118cea98a4290c0e9b56bf60032fac58c98
Kateřina Fořtová <43783793+kateriska@users.noreply.github.com>
2019-10-27 23:09:46 +0100

    UTF 8 to ISO

--- userCourses.php
+++ userCourses.php
@@ -4,11 +4,11 @@ function show_my_courses_garant($user_id, $db)
   $query = "SELECT * FROM kurzy, uzivatele WHERE kurzy.garant_ID = '$user_id' AND uzivatele.Uzivatele_ID = '$user_id'";
   $result = mysqli_query($db, $query);
   if ($result->num_rows > 0) {
-    echo "<b>Vami zastitovane kurzy:</b>";
+    echo "<b>Vámi za¹»i»ované kurzy:</b>";
     echo"<table>";
       echo"<tr>";
         echo"<th>Zkratka kurzu</th>";
-        echo"<th>Nazev kurzu</th>";
+        echo"<th>Název kurzu</th>";
         echo"<th>Typ kurzu</th>";
       echo"</tr>";
     while($row = $result->fetch_assoc())
@@ -28,11 +28,11 @@ function show_my_courses_lecturer($user_id, $db)
   $query = "SELECT * FROM kurzy, terminy, uzivatele WHERE terminy.lektor_ID = '$user_id' AND terminy.Kurzy_ID = kurzy.Kurzy_ID AND terminy.lektor_ID = uzivatele.Uzivatele_ID AND uzivatele.Uzivatele_ID = '$user_id'";
   $result = mysqli_query($db, $query);
   if ($result->num_rows > 0) {
-    echo "<b>Vami vyucovane kurzy:</b>";
+    echo "<b>Vámi vyuèované kurzy:</b>";
     echo"<table>";
       echo"<tr>";
         echo"<th>Zkratka kurzu</th>";
-        echo"<th>Nazev kurzu</th>";
+        echo"<th>Název kurzu</th>";
         echo"<th>Typ kurzu</th>";
       echo"</tr>";
     while($row = $result->fetch_assoc())
@@ -52,11 +52,11 @@ function show_my_courses_student($user_id, $db)
   $query = "SELECT * FROM kurzy, zapsane_kurzy, uzivatele WHERE zapsane_kurzy.student_ID = '$user_id' AND zapsane_kurzy.Kurzy_ID = kurzy.Kurzy_ID AND zapsane_kurzy.student_ID = uzivatele.Uzivatele_ID AND uzivatele.Uzivatele_ID = '$user_id'";
   $result = mysqli_query($db, $query);
   if ($result->num_rows > 0) {
-    echo "<b>Vase zapsane kurzy:</b>";
+    echo "<b>Va¹e zapsané kurzy:</b>";
     echo"<table>";
       echo"<tr>";
         echo"<th>Zkratka kurzu</th>";
-        echo"<th>Nazev kurzu</th>";
+        echo"<th>Název kurzu</th>";
         echo"<th>Typ kurzu</th>";
       echo"</tr>";
     while($row = $result->fetch_assoc())
@@ -74,13 +74,13 @@ function show_my_courses_student($user_id, $db)
 
 /*
 require_once("dbh.php");
-echo "Vypis pro garanta:\n";
+echo "Výpis pro garanta:\n";
 echo "<br />";
 show_my_courses_garant(9, $db);
-echo "Vypis pro lektora:\n";
+echo "Výpis pro lektora:\n";
 echo "<br />";
 show_my_courses_lecturer(4, $db);
-echo "Vypis pro studenta:\n";
+echo "Výpis pro studenta:\n";
 echo "<br />";
 show_my_courses_student(5, $db);
 */
