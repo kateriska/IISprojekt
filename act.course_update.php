68e87c54dd7866132f532dfcf61bdbfc0f615ce5
mazanec <ondrejholub202@gmail.com>
2019-10-31 19:08:05 +0100

    wip course update

--- act.course_update.php
+++ act.course_update.php
@@ -14,13 +14,19 @@ $desc = $_POST['desc'];
 $id = $_POST['id'];
 $draft = $_POST['draftval'];
 
+if(isset($_POST['dep_head'])){
+  $dep_head_text = "vedouci_ID='$dep_head'",
+}else{
+  $dep_head_text = '';
+}
+
 session_start();
 $me = $_SESSION['user_id'];
 if($draft == 0){
   $query = "INSERT INTO ke_schvaleni_kurz (Kurzy_ID, nazev, popis, typ, cena, garant_ID, vedouci_ID, zadatel_ID)
   VALUES ('$id', '$name', '$desc', '$type', '$price', '$garant', '$dep_head', '$me')";
 }else{
-  $query = "UPDATE ke_schvaleni_kurz SET nazev='$name', popis='$desc', typ='$type', cena='$price', garant_ID='$garant', vedouci_ID='$dep_head', zadatel_ID='$me' WHERE Kurzy_ID='$id'";
+  $query = "UPDATE ke_schvaleni_kurz SET nazev='$name', popis='$desc', typ='$type', cena='$price', garant_ID='$garant', $dep_head_text zadatel_ID='$me' WHERE Kurzy_ID='$id'";
 }
 //echo($query);
 
