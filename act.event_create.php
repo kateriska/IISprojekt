b7502b8e4edd88f97a78f6820488332cfd156f69
mazanec <ondrejholub202@gmail.com>
2019-11-03 15:47:54 +0100

    wip terminy

--- act.event_create.php
+++ act.event_create.php
@@ -1,12 +1,12 @@
 <?php
 function room_event($id, $type, $date, $time, $duration, $lector, $desc, $room, $db){
+  echo("$date $time $duration");
   $query = "SELECT datum, cas, mistnosti_ID, Kurzy_ID, `type` FROM terminy WHERE mistnosti_ID='$room' AND datum='$date'";
   $result = mysqli_query($db, $query);
-  if($result == FALSE){
+  /*if($result == FALSE){
     header("Location: ./event_create.php?id=$id&err=sql1");
     exit();
-  }
-  echo("$date $time $duration");
+  }*/
   $isfree = TRUE;
   /*while( $row = mysqli_fetch_assoc($result) ){
     //kontrola prekryvani udalosti
