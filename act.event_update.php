a1dc90bd4ed28132ebb0c4fb2df9a9b3fae592a7
mazanec <ondrejholub202@gmail.com>
2019-11-04 21:27:35 +0100

    wip terminy

--- act.event_update.php
+++ act.event_update.php
@@ -19,13 +19,43 @@ $prev_time = $_POST['prev_time'];
 
 
 if( $id == ''|| $date == '' || $time == ''|| ($duration < '0' && $duration != '') ){
-  header("Location: ./room.php?id=$id&err=empty_or_inv_fields");
+  header("Location: ./event.php?id=$id&d=$date&t=$time&r=$room&err=empty_fields");
   exit();
 }
 
 require_once("dbh.php");
 
-//todo delete, todo insert
+$query = "DELETE FROM terminy WHERE Kurzy_ID='$id' AND datum='$prev_date' AND cas='$prev_time' AND mistnost_ID='$prev_room'";
+$result = mysqli_query($db, $query);
+if($result == FALSE){
+  echo($query);
+  /*header("Location: ./event.php?id=$id&d=$prev_date&t=$prev_time&r=$prev_room&err=sql_del");
+  exit();*/
+}
+
+$query = "INSERT INTO terminy (Kurzy_ID, datum, cas, mistnost_ID, lektor_ID, popis, typ_termin, doba_trvani) 
+          VALUES ('$id', '$date', '$time', '$room', '$lector', '$desc', '$type', '$duration')";
+$result = mysqli_query($db, $query);
+if($result == FALSE){
+  echo($query);
+  $query = "INSERT INTO terminy (Kurzy_ID, datum, cas, mistnost_ID, lektor_ID, popis, typ_termin, doba_trvani) 
+          VALUES ('$id', '$prev_date', '$prev_time', '$prev_room', '$lector', '$desc', '$type', '$duration')";
+  $result = mysqli_query($db, $query);
+  if($result == FALSE){
+    echo($query . " PRUSVIH");
+    /*header("Location: ./event.php?id=$id&d=$prev_date&t=$prev_time&r=$prev_room&err=sql_del");
+    exit();*/
+  }
+  
+  /*header("Location: ./event.php?id=$id&d=$prev_date&t=$prev_time&r=$prev_room&err=sql_del");
+  exit();*/
+}
+
+
+
+
+
+
 
 exit();
 ?>
