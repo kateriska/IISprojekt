df6d30c765265d89832d5b7ac2a75d14fb9977aa
mazanec <ondrejholub202@gmail.com>
2019-11-12 22:58:11 +0100

    O

--- event.php
+++ event.php
@@ -19,8 +19,8 @@
     event_show_info_or_edit($_GET['id'], $_GET['d'], $_GET['t'], $_GET['r']);         //pro zapsane/ editace a mazani vlastnenym lektorum, garantum a vedoucim
     event_delete();                    //vlastnikum krome lektoru
     //get_event_files();               TODO: soubory pro vsechny/registrovane/zapsane
-    event_tile_insert_marks($_GET['id'], $_GET['d'], $_GET['t'], $_GET['r']);
     insert_reverse_tile("Zpìt na kurz ".$_GET['id'], "./course.php?id=".$_GET['id']);
+    event_tile_insert_marks($_GET['id'], $_GET['d'], $_GET['t'], $_GET['r']);
   ?>
   </container>
 </body>
