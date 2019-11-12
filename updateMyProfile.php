71b8ae0c8ec8db7806b99392796385746bd9a702
mazanec <ondrejholub202@gmail.com>
2019-11-12 20:40:38 +0100

    O

--- updateMyProfile.php
+++ updateMyProfile.php
@@ -1,3 +1,21 @@
+<html>
+<head>
+  <link rel="stylesheet" href="style.css">
+  <?php 
+    require_once('fn.elements.php');  
+  ?>
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+</head>
+
+
+<body>
+<?php
+  session_start();  
+  insert_login_bar();
+?> 
+
+<container class="center">
+
 <?php
 
 function get_profile_details($id, $db){
@@ -53,3 +71,8 @@ require_once("fn.pwd_hash.php");
 get_profile_details(1, $db);
 
 ?>
+
+</container>
+</body>
+
+</html>
