6bab6df7045961af7eb5a4d798bd5ad09ba2e0e8
mazanec <ondrejholub202@gmail.com>
2019-11-14 23:50:43 +0100

    o

--- act.marks_submit.php
+++ act.marks_submit.php
@@ -12,15 +12,16 @@ $room = $_POST['room'];
 $me = $_POST['author'];
 
 require("dbh.php");
-for($i=1; $i<$inputs; $i++){
+for($i=1; $i<=$inputs; $i++){
   $student = $_POST["u$i"];
   $marks = $_POST["h$i"];
+  echo("'$marks'");
   $query = "INSERT INTO hodnoceni (Kurzy_ID, datum, cas, mistnost_ID, student_ID, hodnoceni, hodnotil_ID)
                   VALUES ('$course', '$date', '$time', '$room', '$student', '$marks', '$me')";
   mysqli_query($db, $query);
   echo($query);
 }
 
-header("Location: ./marks.php?id=$course&d=$date&t=$time&r=$room");
-exit();
+/*header("Location: ./marks.php?id=$course&d=$date&t=$time&r=$room");
+exit();*/
 ?>
