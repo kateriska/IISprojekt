c011e3e4fe4bde1ec1abc80427638301cdbebaf5
mazanec <ondrejholub202@gmail.com>
2019-11-14 23:42:16 +0100

    o

--- act.marks_submit.php
+++ act.marks_submit.php
@@ -18,8 +18,9 @@ for($i=1; $i<$inputs; $i++){
   $query = "INSERT INTO hodnoceni (Kurzy_ID, datum, cas, mistnost_ID, student_ID, hodnoceni, hodnotil_ID)
                   VALUES ('$course', '$date', '$time', '$room', '$student', '$marks', '$me')";
   mysqli_query($db, $query);
+  echo($query);
 }
 
-header("Location: ./marks.php?id=$course&d=$date&t=$time&r=$room");
-exit();
+/*header("Location: ./marks.php?id=$course&d=$date&t=$time&r=$room");
+exit();*/
 ?>
