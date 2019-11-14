5e072faa65429e4175ed2c23d7e13f575862543f
mazanec <ondrejholub202@gmail.com>
2019-11-15 00:19:13 +0100

    o

--- scripts/fillDatab.php
+++ scripts/fillDatab.php
@@ -3,7 +3,7 @@ $db = mysqli_init();
 $login = "xholub42";
 $password = "n4etimbe";
 
-if (!mysqli_real_connect($db, 'localhost', $login, $password, $login, 0, '/var/run/mysql/mysql.sock')){
+if (!mysqli_real_connect($db, '127.0.0.1', $login, $password, $login, 0, '/var/run/mysql/mysql.sock')){
   die('cannot connect '.mysqli_connect_error());
 }
 function controlProcedures($db, $command, $link, $info)
