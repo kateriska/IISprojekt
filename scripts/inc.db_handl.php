7ded77d0b8f4c0e8492c38758b7fcac711f74fae
mazanec <ondrejholub202@gmail.com>
2019-11-15 00:14:57 +0100

    o

--- scripts/inc.db_handl.php
+++ scripts/inc.db_handl.php
@@ -1,13 +1,7 @@
 <?php
-
-if(!isset($argv[1]) || !isset($argv[2])){
-  echo("error: no args!");
-  exit(0);
-}
-
 $db = mysqli_init();
-$login = $argv[1];
-$password = $argv[2];
+$login = "xholub42";
+$password = "n4etimbe";
 
 if (!mysqli_real_connect($db, 'localhost', $login, $password, $login, 0, '/var/run/mysql/mysql.sock')){
   die('cannot connect '.mysqli_connect_error());
