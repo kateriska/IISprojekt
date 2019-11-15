183e9540d97c622428aa012ab62ba918b2394f8b
Kateřina Fořtová <43783793+kateriska@users.noreply.github.com>
2019-11-15 21:37:08 +0100

    addid new values and users

--- scripts/fillDatab.php
+++ scripts/fillDatab.php
@@ -31,35 +31,55 @@ $uzivatele_insert = 'INSERT INTO uzivatele (Uzivatele_ID, jmeno, prijmeni, heslo
 (7, "Adam", "Hajek", "$2y$09$pyyWLIMoqMlaHeUVcbg6pu9kYginRst0kU4lCPX/zEytVtL/lS2Ci", 1, "hajek@email.cz"),
 (8, "Lucie", "Otahalova", "$2y$09$ht2QP4ilxoBNrzkvdgDF9O.iI6Gx9x2Po4GJWojWlVKATm6uS33T2", 4, "lucie.otahalova@post.cz"),
 (9, "Zaneta", "Formankova", "$2y$09$FnXWZDMfwjOmpv26WlApHeJRnzi8jmk7/tNgOGBbK8kw3bErXTqS2", 3, "formankova@email.cz"),
-(10, "Zuzana", "Stivinova", "$2y$09$5Z6u4IQ1flPP1fXMoRs5NuPdsx2HOOaMN2R6w7j3VRdnK/SqXQida", 2, "stivinova@seznam.cz");';
+(10, "Zuzana", "Stivinova", "$2y$09$5Z6u4IQ1flPP1fXMoRs5NuPdsx2HOOaMN2R6w7j3VRdnK/SqXQida", 2, "stivinova@seznam.cz"),
+(11, "Alena", "Krenova", "$2y$09$cvwm2Y8KiOZj7TX4k8H69eP1MhQpIn4Y017HdnlV4uQfmhAyBHSEG", 1, "student"), -- heslo: student
+(12, "Petr", "Hruby", "$2y$09$ZAgr62guPScYvDMmGR1cZOMAH4/sGGs5LzVklQj5gmbba9wR7EeP.", 2, "lektor"), -- heslo: lektor
+(13, "Marta", "Janska", "$2y$09$7y6oCWp1JdOPln6qq54AUevkc4qZjj3tHy03WCtsct6/j9aLqLF9y", 3, "garant"), -- heslo: garant
+(14, "Ondrej", "Kocour", "$2y$09$nDtAvLGGJHQtKQamTRUVDuG.SqdGsiwtxGI6lrlFaE4V2odraPSgS", 4, "vedouci") -- heslo: vedouci
+;';
 //HESLA: vždy 1. písmeno jména a 3 písmena příjmení (epal, amal,...)
 controlProcedures($db, $uzivatele_insert, $link, "insert into uzivatele");
 
 $kurzy_insert = "INSERT INTO kurzy (Kurzy_ID, nazev, popis, typ, cena, garant_ID, vedouci_ID) VALUES
 ('CADZ', 'Zaklady programu CAD', 'Seznamuje se zaklady modelovani pomoci CAD technologii.', 'Strojirenstvi', 2900, 3, 8),
-('IMD', 'Uvod do medicinskych databazi', 'Seznamuje se zakladem tvorby databazi v medicinskem prostredi, vhodne pro studenty informatiky nebo bionformatiky', 'Medicinska informatika', 5900, 3, 8),
+('ZPR', 'Zaklady programovani', 'Seznamuje se zaklady programovani a algoritmizace.', 'Programovani', 3500, 13 , 14),
+('ZOPP', 'Zaklady OOP', 'Seznamuje se zaklady objektove orientovanych jazyku.', 'Programovani', 4000, 13 , 14),
+('CTZ', 'Citace a psani odbornych textu', 'Seznamuje se spravnym citovanim zdroju a formalnimi potrebami pro psani odbornych textu. Vhodne hlavne pro studenty, co budou psat bakalarskou nebo diplomovou praci.', 'Typografie', 1500, 9 , 14),
+('IMD', 'Uvod do medicinskych databazi', 'Seznamuje se zakladem tvorby databazi v medicinskem prostredi, vhodne pro studenty informatiky nebo bionformatiky.', 'Medicinska informatika', 5900, 3, 8),
 ('VID', 'Zaklady tvorby video-obsahu', 'Vhodne pro zajemce o nataceni videi, kratkych filmu, strihani videa atd.', 'Multimedia', 5000, 9, 8);";
 controlProcedures($db, $kurzy_insert, $link, "insert into kurzy");
 
 $mistnosti_insert = "INSERT INTO mistnosti (Mistnosti_ID, adresa, typ, kapacita) VALUES
 ('F202', 'Manesova 220/3, Brno', 'poslucharna', 100),
+('F208', 'Manesova 220/3, Brno', 'poslucharna', 120),
 ('G505', 'Manesova 220/3, Brno', 'laborator', 30),
+('G507', 'Manesova 220/3, Brno', 'laborator', 30),
+('I007', 'Hutarova 183/6, Brno', 'laborator', 20),
 ('H009', 'Hutarova 183/6, Brno', 'poslucharna', 120);";
 controlProcedures($db, $mistnosti_insert, $link, "insert into mistnosti");
 
 $terminy_insert = "INSERT INTO terminy (Kurzy_ID, datum, cas, mistnost_ID, lektor_ID, popis, typ_termin, kapacita, doba_trvani, schvaleno) VALUES
 ('CADZ', '2019-10-18', '12:30:00.000000', 'F202', 4, 'Konani seminare. Vizte web stranky www.kurzy/cadz.pdf s uvodnim materialem', 'prednaska', 60, 120, 'ne'),
 ('CADZ', '2019-11-30', '15:00:00.000000', '', 8, 'Domaci ukol k pouziti zakladnich prikazu. Vizte sablonu na odkaze: www.cad.cz/sablona.pdf. Ukol bude opravovan uvedenym lektorem.', 'domaci ukol', 50, 0,'ne'),
+('ZOPP', '2019-12-01', '15:00:00.000000', 'G507', 4, 'Konani seminare. Prosim zopakujte si/nastudujte zaklady jazyka PHP a Python.', 'cviceni', 15, 120,'ne'),
 ('IMD', '2019-11-28', '15:00:00.000000', 'G505', 4, 'Cviceni v laboratori. Budete seznameni s laboratornim radem. Prosim, noste si psaci potreby a pripadne notebook.', 'cviceni', 20, 120,'ne'),
+('ZPR', '2019-12-16', '13:00:00.000000', 'H009', 12, 'Cviceni v laboratori. Budete seznameni s laboratornim radem.', 'cviceni', 20, 120,'ne'),
+('CTZ', '2019-12-10', '18:00:00.000000', 'F208', 12, 'Uvodni hodina. Popovidame si hlavne o citacnich normach ISO.', 'prednaska', 120, 120,'ne'),
+('CTZ', '2019-12-18', '20:00:00.000000', '', 12, 'Bodovany ukol, vizte na pripravu https://www.knihovnazn.cz/files/citace.pdf', 'domaci ukol', 120, '','ne'),
 ('VID', '2019-10-28', '11:00:00.000000', 'H009', 8, 'Prednaska k problematice a demonstrace modeloveho prikladu a programu. Odkazy ke stazeni probirane latky budou doplneny.', 'prednaska', 90, 60,'ne');";
 controlProcedures($db, $terminy_insert, $link, "insert into terminy");
 
 $zapsane_kurzy_insert = "INSERT INTO zapsane_kurzy (Kurzy_ID, student_ID) VALUES
 ('VID',5),
 ('VID',7),
+('CTZ',7),
 ('IMD',5),
+('IMD',11),
+('ZPR',11),
 ('CADZ',6),
+('ZOPP',10),
 ('VID',4),
+('CTZ',4),
 ('VID',6);";
 controlProcedures($db, $zapsane_kurzy_insert, $link, "insert into zapsane_kurzy");
 
@@ -69,7 +89,8 @@ $soubory_insert = "INSERT INTO soubory (Kurzy_ID, datum, cas, mistnost_ID, nazev
 controlProcedures($db, $soubory_insert, $link, "insert into soubory");
 
 $ke_schvaleni_kurz_insert = "INSERT INTO ke_schvaleni_kurz (Kurzy_ID, nazev, popis, typ, cena, garant_ID, vedouci_ID, zadatel_ID) VALUES
-('CADZ', 'Zaklady programu CAD', 'Seznamuje se zaklady modelovani pomoci CAD technologii. A tohle tam pridej', 'Strojirenstvi', 2900, 3, 8, 3);";
+('CTZ', 'Citace a psani odbornych textu', 'Seznamuje se spravnym citovanim zdroju a formalnimi potrebami pro psani odbornych textu. Vhodne hlavne pro studenty, co budou psat bakalarskou nebo diplomovou praci.', 'Typografie', 2000, 9, 14, 9),
+('CADZ', 'Zaklady programu CAD', 'Seznamuje se zaklady modelovani pomoci CAD technologii. Vhodne zvlaste pro zajemce z fakult strojnich.', 'Strojirenstvi', 3500, 3, 8, 3);";
 controlProcedures($db, $ke_schvaleni_kurz_insert, $link, "insert into ke_schvaleni_kurz");
 
 $ke_schvaleni_student_insert = "INSERT INTO ke_schvaleni_student (Kurzy_ID, student_ID) VALUES
@@ -79,6 +100,10 @@ controlProcedures($db, $ke_schvaleni_student_insert, $link, "insert into ke_schv
 
 $hodnoceni_insert = "INSERT INTO hodnoceni (Kurzy_ID, datum, cas, mistnost_ID, student_ID, hodnoceni, hodnotil_ID) VALUES
 ('CADZ', '2019-10-18', '12:30:00.000000', 'F202', 6, 20, 4),
-('CADZ', '2019-11-30', '15:00:00.000000', '', 6, 15, 8);";
+('VID', '2019-10-28', '11:00:00.000000', 'H009', 6, 60, 8),
+('CTZ', '2019-12-18', '20:00:00.000000', '', 4, 30, 12),
+('CTZ', '2019-12-18', '20:00:00.000000', '', 7, 20, 12),
+('CADZ', '2019-11-30', '15:00:00.000000', '', 6, 15, 8),
+('IMD', '2019-11-28', '15:00:00.000000', 'G505', 11, 18, 4);";
 controlProcedures($db, $hodnoceni_insert, $link, "insert into hodnoceni");
 ?>
