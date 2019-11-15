8a826ea385228ecc4b18920e289d5d11af67110d
mazanec <ondrejholub202@gmail.com>
2019-11-15 12:08:03 +0100

    o

---
+++ db.actions.php
@@ -0,0 +1,11 @@
+<html>
+<head></head>
+<body>
+<form method='get' action='scripts/initDatab.php'>
+<button type='submit' value='submit-init'>INIT DB</button>
+</form>
+<form method='get' action='scripts/fillDatab.php'>
+<button type='submit' value='submit-fill'>FILL DB</button>
+</form>
+</body>
+</html>
