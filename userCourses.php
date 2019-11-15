757dcaab2c24ff3ba62d189c20fd101b371fddce
Kateřina Fořtová <43783793+kateriska@users.noreply.github.com>
2019-11-15 15:12:32 +0100

    merge hodnoceni to vase zapsane kurzy

--- userCourses.php
+++ userCourses.php
@@ -45,6 +45,22 @@ function show_my_courses_lecturer($user_id, $db)
 }
 function show_my_courses_student($user_id, $db)
 {
+  $query = "SELECT Kurzy_ID, SUM(hodnoceni) hodnoceniSum FROM hodnoceni WHERE hodnoceni.student_ID = '$user_id' GROUP BY Kurzy_ID";
+  $result = mysqli_query($db, $query);
+  if ($result->num_rows > 0) {
+    while($row = $result->fetch_assoc())
+    {
+      $course_id =  htmlspecialchars($row['Kurzy_ID']);
+      $hodnoceni = htmlspecialchars($row['hodnoceniSum']);
+      $hodnoceni_arr[$course_id] = $hodnoceni;
+      $some_hodnoceni = true;
+    }
+  }
+  else
+  {
+    $some_hodnoceni = false;
+  }
+
   $query = "SELECT * FROM kurzy, zapsane_kurzy, uzivatele WHERE zapsane_kurzy.student_ID = '$user_id' AND zapsane_kurzy.Kurzy_ID = kurzy.Kurzy_ID AND zapsane_kurzy.student_ID = uzivatele.Uzivatele_ID AND uzivatele.Uzivatele_ID = '$user_id'";
   $result = mysqli_query($db, $query);
   if ($result->num_rows > 0) {
@@ -54,36 +70,36 @@ function show_my_courses_student($user_id, $db)
         echo"<th>Zkratka kurzu</th>";
         echo"<th>Název kurzu</th>";
         echo"<th>Typ kurzu</th>";
+        echo"<th>Hodnocení</th>";
       echo"</tr>";
     while($row = $result->fetch_assoc())
     {
       $course_id =  htmlspecialchars($row['Kurzy_ID']);
       $nazev =  htmlspecialchars($row['nazev']);
       $typ =  htmlspecialchars($row['typ']);
-      echo "<tr><td><b>$course_id</b></td><td><a href='./course?id=$course_id'>$nazev</a></td><td>$typ</td></tr>";
+      if ($some_hodnoceni == true)
+      {
+        $keys = array_keys($hodnoceni_arr);
+        foreach($keys as $key)
+        {
+          if ($course_id == $key)
+          {
+            $body = $hodnoceni_arr[$key];
+          }
+        }
+
+        echo "<tr><td><b>$course_id</b></td><td><a href='./course?id=$course_id'>$nazev</a></td><td>$typ</td></td><td>$body</td></tr>";
+      }
+      else
+      {
+        $body = 0;
+        echo "<tr><td><b>$course_id</b></td><td><a href='./course?id=$course_id'>$nazev</a></td><td>$typ</td></td><td>$body</td></tr>";
+      }
     }
     echo "</table><br>";
   }
 
 
-
-  $query = "SELECT Kurzy_ID, SUM(hodnoceni) hodnoceniSum FROM hodnoceni WHERE hodnoceni.student_ID = '$user_id' GROUP BY Kurzy_ID";
-  $result = mysqli_query($db, $query);
-  if ($result->num_rows > 0) {
-    echo "<b>Va¹e zí­skané body ze zapsaných kurzù:</b>";
-    echo"<table>";
-      echo"<tr>";
-        echo"<th>Zkratka kurzu</th>";
-        echo"<th>Celkové body</th>";
-      echo"</tr>";
-    while($row = $result->fetch_assoc())
-    {
-      $course_id =  htmlspecialchars($row['Kurzy_ID']);
-      $hodnoceni = htmlspecialchars($row['hodnoceniSum']);
-      echo "<tr><td><a href='./course?id=$course_id'>$course_id</a></td><td>$hodnoceni</td></tr>";
-    }
-    echo "</table><br>";
-  }
 }
 
 function show_my_courses_vedouci($user_id, $db)
