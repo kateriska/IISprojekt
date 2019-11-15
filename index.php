d5ab57adc295b9ecb33b6c0227d69c9df3f3df9a
Kateřina Fořtová <43783793+kateriska@users.noreply.github.com>
2019-11-15 18:53:02 +0100

    control of empty id

--- index.php
+++ index.php
@@ -1,9 +1,9 @@
 <html>
 <head>
   <link rel="stylesheet" href="style.css">
-  <?php 
+  <?php
     require_once('fn.elements.php');
-    require_once('userCourses.php');  
+    require_once('userCourses.php');
   ?>
   <meta name="viewport" content="width=device-width, initial-scale=1">
 </head>
@@ -11,14 +11,14 @@
 
 <body>
 <?php
-  session_start();  
+  session_start();
   insert_login_bar();
 ?>
 
   <container class="center">
   <h1>Hlavní stránka</h1>
-<?php 
-  
+<?php
+
   require_once('dbh.php');
   if(isset($_SESSION['user_id'])){
     $id = $_SESSION['user_id'];
@@ -27,11 +27,14 @@
     show_my_courses_student($id, $db);                  //S
     show_my_courses_lecturer($id, $db);                 //L
     show_my_courses_garant($id, $db);                   //G
-    tile_edit_profile($id);                        //S
   }
   tile_manage_rooms();                        //M
   tile_manage_users();                        //A
   tile_show_all_courses();                    //N
+  if (isset($_SESSION['user_id']))
+  {
+    tile_edit_profile($id);                        //S
+  }
 ?>
   </container>
 </body>
