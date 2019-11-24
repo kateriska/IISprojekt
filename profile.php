cf4cf410115303b6f3fecbf5def29a8fc2625ab2
Kateřina Fořtová <43783793+kateriska@users.noreply.github.com>
2019-11-24 22:39:48 +0100

    repair

--- profile.php
+++ profile.php
@@ -19,6 +19,23 @@
 <?php
 
 function get_profile_details($id, $db){
+  $mails_all_users = array();
+
+  $query = "SELECT email FROM uzivatele";
+  $result = mysqli_query($db, $query);
+  if($result === FALSE){ //SQL ERR
+    echo("CHYBA SQL");return;
+  }
+
+
+  if ($result->num_rows > 0) {
+    while($row = $result->fetch_assoc())
+    {
+      $mail =  htmlspecialchars($row['email']);
+      array_push($mails_all_users, $mail );
+    }
+  }
+
   $query = "SELECT jmeno, prijmeni, email FROM uzivatele WHERE Uzivatele_ID='$id'";
   $result = mysqli_query($db, $query);
   if($result === FALSE){ //SQL ERR
@@ -30,36 +47,82 @@ function get_profile_details($id, $db){
   $firstname = htmlspecialchars($row['jmeno']);
   $lastname = htmlspecialchars($row['prijmeni']);
   $mail = htmlspecialchars($row['email']);
-  $r_str = "<h2>Upravit své údaje</h2><form action=profile.php method='post'>
-              Jméno*:<br><input type='text' name='firstname' value='$firstname'><br>
-              Pøíjmení*:<br><input type='text' name='lastname' value='$lastname'><br>
-              Email*:<br><input type='text' name='mail' value='$mail'><br>
-              Heslo*:<br><input type='text' name='pwd'><br>
+  $r_str = "<h2>Upravit údaje</h2><form action=profile.php method='post'>
+              Jméno:<br><input type='text' name='firstname' value='$firstname'><br>
+              Pøíjmení½:<br><input type='text' name='lastname' value='$lastname'><br>
+              Email:<br><input type='text' name='mail' value='$mail'><br>
               <button type='submit' name='submitbutton'>Potvrdit zmìny</button>
             </form>";
   echo($r_str);
 
 
-  if ((isset($_POST['submitbutton'])) && (isset($_POST['firstname'])) && (isset($_POST['lastname'])) && (isset($_POST['mail'])) && (isset($_POST['pwd'])))
+  if ((isset($_POST['submitbutton'])) && (isset($_POST['firstname'])) && (isset($_POST['lastname'])) && (isset($_POST['mail'])))
   {
     $firstname= $_POST['firstname'];
     $lastname= $_POST['lastname'];
     $mail = $_POST['mail'];
-    $pwd= $_POST['pwd'];
     $submitbutton= $_POST['submitbutton'];
-    if (empty($pwd))
-    {
-      $query = "UPDATE uzivatele SET jmeno='$firstname', prijmeni='$lastname', email='$mail' WHERE Uzivatele_ID='$id'";
 
-      mysqli_query($db, $query);
+    if ($firstname == '' || $lastname == '' || $mail == '' )
+    {
+        header("Location: ./profile.php?id=$id&err=empty_field");
+        exit();
+    }
+    if (in_array($mail, $mails_all_users))
+    {
+      header("Location: ./profile.php?id=$id&err=mail_already_used");
+      exit();
+    }
+
+    $query = "UPDATE uzivatele SET jmeno='$firstname', prijmeni='$lastname', email='$mail' WHERE Uzivatele_ID='$id'";
+
+    mysqli_query($db, $query);
+
+    if (mysqli_query($db, $query))
+    {
+      header("Location: ./profile.php?id=$id&succ=created");
     }
     else
     {
-      $pwd_hash = pwd_crypt($pwd);
-
-      $query = "UPDATE uzivatele SET jmeno='$firstname', prijmeni='$lastname', email='$mail', heslo='$pwd_hash' WHERE Uzivatele_ID='$id'";
-
+      header("Location: ./profile.php?id=$id&err=empty_field");
     }
+
+
+    exit();
+
+  }
+
+  $query = "SELECT heslo FROM uzivatele WHERE Uzivatele_ID='$id'";
+  $result = mysqli_query($db, $query);
+  if($result === FALSE){ //SQL ERR
+    echo("CHYBA SQL");return;
+  }
+
+  $row = mysqli_fetch_assoc($result);
+
+  $pwd = htmlspecialchars($row['heslo']);
+
+  $p_str = "<h2>Upravit heslo</h2><form action=profile.php method='post'>
+            Heslo:<br><input type='text' name='pwd'><br>
+            <button type='submit' name='submitpwdbutton'>Potvrdit heslo</button>
+          </form>";
+  echo($p_str);
+
+  if (isset($_POST['pwd']) && (isset($_POST['submitpwdbutton'])))
+  {
+    $pwd= $_POST['pwd'];
+    $submitbutton= $_POST['submitpwdbutton'];
+
+    if ($pwd == '')
+    {
+        header("Location: ./profile.php?id=$id&err=empty_field");
+        exit();
+    }
+
+
+    $pwd_hash = pwd_crypt($pwd);
+
+    $query = "UPDATE uzivatele SET heslo='$pwd_hash' WHERE Uzivatele_ID='$id'";
 
     if (mysqli_query($db, $query))
     {
@@ -71,16 +134,18 @@ function get_profile_details($id, $db){
     }
 
     exit();
+}
 
-  }
+
+
 }
 
 require_once("dbh.php");
 require_once("fn.pwd_hash.php");
 
-
-get_profile_details($_GET['id'], $db);
-insert_reverse_tile("Zpìt na hlavní pøehled", "./index.php");
+$id = isset($_SESSION['user_id']);
+get_profile_details($id, $db);
+//get_profile_details(1, $db);
 
 
 
