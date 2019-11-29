b1f419c24ca1caf751ff4b2793ec6583f7b21d20
Kateřina Fořtová <43783793+kateriska@users.noreply.github.com>
2019-11-29 21:34:30 +0100

    insert file

--- act.event_update.php
+++ act.event_update.php
@@ -23,7 +23,31 @@ if( $id == ''|| $date == '' || $time == ''|| ($duration < '0' && $duration != ''
   exit();
 }
 
+if(isset($_FILES['input_file'])){
+
+   $errors= array();
+   $file_name = $_FILES['input_file']['name'];
+   $file_size = $_FILES['input_file']['size'];
+   $file_tmp = $_FILES['input_file']['tmp_name'];
+   $file_type = $_FILES['input_file']['type'];
+   $file_ext=strtolower(end(explode('.',$_FILES['input_file']['name'])));
+
+   if($file_size > 52428800) {
+      $errors[]='Chyba - Soubor je vìt¹í jak 50 MB!';
+   }
+
+   if(empty($errors)==true) {
+      chmod("event_files/".$file_tmp, 644);
+      move_uploaded_file($file_tmp,"event_files/".$file_name);
+      $file_upload = true;
+   }else{
+      print_r($errors);
+   }
+}
+
 require_once("dbh.php");
+
+
 
 $query = "DELETE FROM terminy WHERE Kurzy_ID='$id' AND datum='$prev_date' AND cas='$prev_time' AND mistnost_ID='$prev_room'";
 $result = mysqli_query($db, $query);
@@ -33,12 +57,12 @@ if($result == FALSE){
   exit();
 }
 
-$query = "INSERT INTO terminy (Kurzy_ID, datum, cas, mistnost_ID, lektor_ID, popis, typ_termin, doba_trvani) 
+$query = "INSERT INTO terminy (Kurzy_ID, datum, cas, mistnost_ID, lektor_ID, popis, typ_termin, doba_trvani)
           VALUES ('$id', '$date', '$time', '$room', '$lector', '$desc', '$type', '$duration')";
 $result = mysqli_query($db, $query);
 if($result == FALSE){
   echo($query);
-  $query = "INSERT INTO terminy (Kurzy_ID, datum, cas, mistnost_ID, lektor_ID, popis, typ_termin, doba_trvani) 
+  $query = "INSERT INTO terminy (Kurzy_ID, datum, cas, mistnost_ID, lektor_ID, popis, typ_termin, doba_trvani)
           VALUES ('$id', '$prev_date', '$prev_time', '$prev_room', '$lector', '$desc', '$type', '$duration')";
   $result = mysqli_query($db, $query);
   if($result == FALSE){
@@ -46,12 +70,23 @@ if($result == FALSE){
     header("Location: ./event.php?id=$id&d=$prev_date&t=$prev_time&r=$prev_room&err=sql_ins");
     exit();
   }
-  
+
   header("Location: ./event.php?id=$id&d=$prev_date&t=$prev_time&r=$prev_room&err=room_occupied");
   exit();
 }
 
+if ($file_upload == true)
+{
+  $query = "INSERT INTO soubory (Kurzy_ID, datum, cas, mistnost_ID, nazev_souboru)
+          VALUES ('$id', '$date', '$time', '$room', '$file_name')";
+  $result = mysqli_query($db, $query);
+  if($result == FALSE){
+    echo "Chyba vkladani souboru";
+  }
+
+}
 header("Location: ./course.php?id=$id&succ=edited");
 exit();
-?>
 
+
+?>
